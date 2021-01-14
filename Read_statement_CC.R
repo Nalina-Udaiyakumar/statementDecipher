179345c728e06bf8872d51951848428456141afd
NU <nalina.u@gmail.com>
2021-01-14 17:46:40 -0500

    Update Read_statement_CC.R
    Updates: 1. Splitting the table columns after the transaction and posting date and before the $ sign
                   2. Writing the transactions table to an excel file

--- Read_statement_CC.R
+++ Read_statement_CC.R
@@ -6,12 +6,12 @@
 # sessionInfo()
 
 #### Library calls -----------
-## installations: uncomment if needed
 # install.packages("pdftools")
 # install.packages("tidyverse")
 # install.packages("lubridate")
 # install.packages("stringr")
 # install.packages("plyr")
+# install.packages("xlsx")
 
 
 library(pdftools)
@@ -19,18 +19,20 @@ library(tidyverse)
 library(lubridate)
 library(stringr)
 library(plyr)
+library(xlsx)
 
 
 #### ---------- Reading the credit card statements
 
 # Setting directory to the credit card statements folder
-setwd('your directory path here')
-## !!!! Don't forget to put your directory path here before you execute!!!
+# setwd('Your directory path)
+# !! Don't forget to put your directory path above!!
 
 # Listing files in the directory 
 filesInDir <- file.info(list.files(full.names = T))
 # Getting the file name with the most recent modified time
 recentFileName <- rownames(filesInDir)[which.max(filesInDir$mtime)]
+print(recentFileName)
 
 # Reading in the most recent credit card statement pdf file 
 creditCardStatement <- pdf_text(recentFileName)
@@ -50,16 +52,20 @@ creditCardStatementDf <- data.frame()
 for(i in 1:(length(creditCardStatement)))
 {
   currentCCStatementPage <- creditCardStatement[i] %>%
-    readr::read_lines()                      ##---- replace 1 with i
+    readr::read_lines()                      
   
   ## Fixing issue with a particular transaction line
   currentCCStatementPage <- currentCCStatementPage %>%
     str_replace(pattern = "PAYMENT - THANK YOU",
-                replacement = "    PAYMENT - THANK YOU") %>%
-    strsplit(split="    ") 
+                replacement = "    PAYMENT - THANK YOU")
   
+  ## ! Update: splitting after deliberately inserting spaces after transaction and posting dates
+  # currentCCStatementPage <- currentCCStatementPage %>%
+  #   strsplit(split="    ") 
+   
   # Flagging rows of the credit card statement page that have transaction data
-  rowsToKeep <- apply(Vectorize(grepl, "pattern")(monthNamesList, str_sub(sapply(currentCCStatementPage, "[[", 1),start=1,end=3)),
+  rowsToKeep <- apply(Vectorize(grepl, "pattern")
+                      (monthNamesList,str_sub(sapply(currentCCStatementPage, "[[", 1),start=1,end=3)),
                       1,
                       function(x) which(x)[1])
   rowsToKeep <- !is.na(rowsToKeep)
@@ -70,6 +76,96 @@ for(i in 1:(length(creditCardStatement)))
   ### Breaking out of the loop if the page has no transactions
   if(length(transactionTable)==0)
     break
+  
+  ## Insert spaces "    " wherever a particular column ends: 
+  ##  after each date in 'MMM dd ' format- i.e transaction date and posting date, before $/-$ sign, 
+  ##  and before the word "PAYMENT", since there is no space in this particular case
+  transactionTable <- transactionTable %>%
+    str_replace(pattern = "PAYMENT - THANK YOU",
+                replacement = "    PAYMENT - THANK YOU")
+  
+  ## Inserting spaces in strings after transaction date: 
+  # Creating regex to indentify the position of transaction date in a string
+  transactionDateFormat <- paste(monthNamesList,collapse=" [0-9]{2} |")
+  transactionDateFormat <- paste0(transactionDateFormat," [0-9]{2} ")
+  transactionDateFormat <- regex(transactionDateFormat)
+  
+  # Locate the position of the posting date
+  # Convert to data frame for ease of operation
+  transactionTable1 <- data.frame(transactionTable)
+  # colnames(transactionTable1)
+  
+  # Extracting the positions of the posted date as columns in the dataframe
+  transactionTable1$transactionDatePosition2start <- 
+    lapply(str_locate_all(transactionTable1$transactionTable,transactionDateFormat),
+           function(x) as.integer(x[2,1]))
+  transactionTable1$transactionDatePosition2end <- 
+    lapply(str_locate_all(transactionTable1$transactionTable,transactionDateFormat),
+           function(x) as.integer(x[2,2]))
+  
+  transactionTable1$transactionDatePosition2beforestart <- as.integer(transactionTable1$transactionDatePosition2start)-1
+  transactionTable1$transactionDatePosition2afterend <- as.integer(transactionTable1$transactionDatePosition2end)+1
+    
+  ## Removing rows that do not have any posting date
+  transactionTable1 <- transactionTable1[!is.na(transactionTable1$transactionDatePosition2end),]
+  
+  # Insert "    " after posting date
+  transactionTable1$correctedData <- paste(str_sub(transactionTable1$transactionTable,
+                                                   start = 1,
+                                                   end = transactionTable1$transactionDatePosition2beforestart),
+                                          str_sub(transactionTable1$transactionTable,
+                                                   start = transactionTable1$transactionDatePosition2start,
+                                                   end = transactionTable1$transactionDatePosition2end),
+                                           "    ",
+                                           str_sub(transactionTable1$transactionTable,
+                                                   start = transactionTable1$transactionDatePosition2afterend,
+                                                   end = str_length(transactionTable1$transactionTable))
+                                           )
+  
+  ## The starting and ending position of the transaction date are always 1 and 7  -!! Scope to update this at a future time
+  # Insert "    " after transaction date
+  transactionTable1$correctedData <- paste(str_sub(transactionTable1$correctedData,
+                                                                   start = 1,
+                                                                   end = 7),
+                                           "    ",
+                                          str_sub(transactionTable1$correctedData,
+                                                                   start = 8,
+                                                                   end = str_length(transactionTable1$correctedData))
+                                                 )
+  
+  
+  ## Inserting spaces in strings before the $/-$ sign: 
+  # Creating regex to indentify the position of transaction date in a string
+  transactionAmountFormat <- "\\$[0-9]{1}"
+  transactionAmountFormat <- regex(transactionAmountFormat)
+  
+  # Locate the position of the $ sign
+  transactionTable1$transactionAmountPositionstart <- 
+    lapply(str_locate_all(transactionTable1$correctedData,transactionAmountFormat),
+           function(x) as.integer(x[1,1]))
+  
+  transactionTable1$transactionAmountPositionbeforestart <- as.integer(transactionTable1$transactionAmountPositionstart)-2
+  transactionTable1$transactionAmountPositionafterstart <- as.integer(transactionTable1$transactionAmountPositionstart)+1
+ 
+  # Insert "    " before the $ or -$ sign
+  transactionTable1$correctedData <- paste(str_sub(transactionTable1$correctedData,
+                                                   start = 1,
+                                                   end = transactionTable1$transactionAmountPositionbeforestart),
+                                           "    ",
+                                           str_sub(transactionTable1$correctedData,
+                                                   start = transactionTable1$transactionAmountPositionafterstart,
+                                                   end = str_length(transactionTable1$correctedData))
+                                           )
+  
+  # Converting transactionTable back to a list
+  transactionTable <- as.list.data.frame(transactionTable1$correctedData)
+  
+  # Removing the unwanted data frame transactionTable1
+  rm(transactionTable1)
+  
+  # Splitting by "    " to get the 4 columns of data in the transaction table
+  transactionTable <- transactionTable %>%
+    strsplit(split="    ") 
   
   # Flagging all elements with zero length
   elementsToKeep <- sapply(transactionTable,function(el) as.integer(as.logical(str_length(el))))
@@ -121,3 +217,9 @@ for(i in 1:(length(creditCardStatement)))
   creditCardStatementDf <- rbind.data.frame(creditCardStatementDf,transactiondf)
 
 }
+# completeListOfTransactions <- creditCardStatementDf ## only when reading the first file
+completeListOfTransactions <- rbind.data.frame(completeListOfTransactions,creditCardStatementDf)
+
+# Writing the complete list of transactions to an excel file
+write.xlsx(completeListOfTransactions, "Credit Card Statements.xlsx", sheetName = "Sheet1",
+           col.names = TRUE, row.names = TRUE, append = FALSE)
